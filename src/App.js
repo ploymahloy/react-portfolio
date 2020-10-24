1843a4a0b3b7f4e82debc1546cc59865c678cdc3
Patrick Mahloy <PatrickMahloy@Patricks-MBP.fios-router.home>
2020-10-24 00:44:47 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import './App.css';
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
       <div>
         <Header/>
-        <Switch>
-          <Route exact path={"/"} component={About}/>
-          <Route exact path={"/about"} component={About}/>
-          <Route exact path={"/portfolio"} component={Portfolio}/>
-          <Route exact path={"/contact"} component={Contact}/>
-        </Switch>
-        {/* <Footer/> */}
+          <Switch>
+            <Route exact path={"/"} component={About}/>
+            <Route exact path={"/about"} component={About}/>
+            <Route exact path={"/portfolio"} component={Portfolio}/>
+            <Route exact path={"/contact"} component={Contact}/>
+          </Switch>
+        <Footer/>
       </div>
     </Router>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(props) {
+    return (
+    <div>
+      Patrick Mahloy © 2020
+    </div>
+    )
+}
+
+export default Footer;
