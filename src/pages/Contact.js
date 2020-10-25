c5e3335fc2488f75a0025d58c240662c4b1a56f0
Patrick Mahloy <PatrickMahloy@Patricks-MBP.fios-router.home>
2020-10-25 01:15:51 -0400

    contact links replaced with linked images

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,15 @@
 import React from "react";
+import "./Contact.css";
 
 function Contact(props) {
     return (
         <div>
-            <a href="https://github.com/ploymahloy">GitHub</a> <br></br>
-            <a href="https://www.linkedin.com/in/patrickmahloy/">LinkedIn</a>
+            <a href="https://github.com/ploymahloy">
+                <img src="../img/github.png"/>
+            </a> 
+            <a href="https://www.linkedin.com/in/patrickmahloy/">
+                <img src="../img/linkedin.png"/>
+            </a>
         </div>
     );
 };
