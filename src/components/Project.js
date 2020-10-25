a3c65be47a6d39024e09cb7f325aa783dfcb733c
Patrick Mahloy <PatrickMahloy@Patricks-MBP.fios-router.home>
2020-10-24 23:48:50 -0400

    styled project images

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Project.css";
 
 function Project(props) {
     return (
     <div>
         <h2>Project Name: </h2> <h4>{props.name}</h4>
         <a>
-            <img src={props.image} alt={props.name}></img>
+            <img src={props.image} alt={props.name} className="img"></img>
         </a>
         <p>{props.description}</p>
     </div>
